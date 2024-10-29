fce33ea4baed45270f57a97a955574ea3d8099a2
Tom Back <tom.back@etu.minesparis.psl.eu>
2024-10-29 15:20:03 +0100

    Ecriture fonction grille

--- main.py
+++ main.py
@@ -20,4 +20,11 @@ def grid(l,c):
 
 
 def grid_layout(grid, pieces):
-
+    l,w = grid.shape
+    grid_color = np.zeroes((l,w,3))
+    for i,ligne in enumerate(grid):
+        for j,pixel in enumerate(ligne):
+            if pixel == 1:
+                grid_color[i,j] == [0,0,0]
+    plt.imshow(grid_color, interpolation='nearest')
+    
