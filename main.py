9fc82bfff98428ce3d4a7f144981de6cf71ce983
Tom Back <tom.back@etu.minesparis.psl.eu>
2024-10-29 15:46:00 +0100

    avancement de l'affichage

--- main.py
+++ main.py
@@ -16,6 +16,21 @@ SHAPE = {'F':np.array([[0,0,0,0,0],[0,0,0,0,0],[0,1,1,0,0],[1,1,0,0,0],[0,1,0,0,
 'Z':np.array([[0,0,0,0,0],[0,0,0,0,0],[1,1,0,0,0],[0,1,0,0,0],[0,1,1,0,0]])
 }
 
+color_palette = [
+    "red",
+    "blue",
+    "green",
+    "yellow",
+    "orange",
+    "purple",
+    "cyan",
+    "magenta",
+    "lime",
+    "pink",
+    "teal",
+    "brown"
+]
+
 def grid(lignes, colonnes, obstacles):
     """Création de la grille"""
     grille = np.zeros((lignes, colonnes))
@@ -27,12 +42,20 @@ def grid(lignes, colonnes, obstacles):
 
 def grid_layout(grid, pieces):
     l,w = len(grid), len(grid[0])
-    grid_color = np.zeroes((l,w,3))
+    grid_color = np.zeros((l,w,3))
+    #On remplit les cases obstacles
     for i,ligne in enumerate(grid):
         for j,pixel in enumerate(ligne):
             if pixel == 1:
-                grid_color[i,j] == [0,0,0]
-    plt.imshow(grid_color, interpolation='nearest')
+                grid_color[i,j] = [0,0,0]
+            else:
+                grid_color[i,j] = [255,255,255]
+    #On remplit les cases occupées par des pièces 
+    for i,piece in enumerate(pieces):
+        piece_name = sum(piece[i]*i+1)
 
-#grid_layout(grid(5,5,[]),[])
+    plt.imshow(grid_color, interpolation='nearest')
+    plt.show()
+
+grid_layout(grid(5,5,[]),[])
 
