43397a0e9200a8e759376d52d6ae3b12d0f378cc
Tom Back <tom.back@etu.minesparis.psl.eu>
2024-10-29 15:28:50 +0100

    commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-import xcover 
+#import xcover 
 import matplotlib.pyplot as plt
 
 SHAPE = {'F':np.array([[0,0,0,0,0],[0,0,0,0,0],[0,1,1,0,0],[1,1,0,0,0],[0,1,0,0,0]]),
@@ -18,34 +18,21 @@ SHAPE = {'F':np.array([[0,0,0,0,0],[0,0,0,0,0],[0,1,1,0,0],[1,1,0,0,0],[0,1,0,0,
 
 def grid(lignes, colonnes, obstacles):
     """Création de la grille"""
-    grille = np.array((lignes, colonnes))
+    grille = np.zeros((lignes, colonnes))
     for (x,y) in obstacles :
         grille[x][y] = 1
     return grille
 
 
 
-def grid(l,c):
-    return np.zeroes((l,c))
-
-
-
-
-
-
-
-
-
-
-
-
-
 def grid_layout(grid, pieces):
-    l,w = grid.shape
+    l,w = len(grid), len(grid[0])
     grid_color = np.zeroes((l,w,3))
     for i,ligne in enumerate(grid):
         for j,pixel in enumerate(ligne):
             if pixel == 1:
                 grid_color[i,j] == [0,0,0]
     plt.imshow(grid_color, interpolation='nearest')
-    
+
+#grid_layout(grid(5,5,[]),[])
+
